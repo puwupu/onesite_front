e1687f3848cf3ce1c9deaaac3e9a0615fbfbed39
herui <rui.he@haiziwang.com>
2021-04-08 10:52:28 +0800

    fix err

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -47,7 +47,7 @@ const actions = {
     return new Promise((resolve, reject) => {
       getInfo()
         .then(response => {
-          const { data } = response;
+          const { data } = response.data;
 
           if (!data) {
             reject("Verification failed, please Login again.");
